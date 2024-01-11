c2ac9bc91603dd3de75ff0f1e6dcc54148a3aa09
Mr_Y <39675193+Scorpion7812@users.noreply.github.com>
2024-01-11 12:08:04 +0000

    modified:   package-lock.json 	modified:   src/app/account/wallet/page.tsx

--- src/app/account/wallet/page.tsx
+++ src/app/account/wallet/page.tsx
@@ -1,7 +1,7 @@
 import { OrderlyConfigProvider } from "@orderly.network/hooks";
-import "./style.css";
-import { WalletConnectProvider } from "/home/yuthish7812/project1/orderly/ordery-trade/src/app/lib/connectwallet.tsx";
-import { wallet } from '../../../../../../sniperbot/raydium-sdk-V1-demo/config';
+import { WalletConnectProvider } from "../../lib/connectwallet";
+
+
 
 export default function Page() {
   return (
