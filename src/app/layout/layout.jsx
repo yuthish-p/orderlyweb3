abb014fb605e10209c81353cf5e5e4755cff901b
Mr_Y <39675193+Scorpion7812@users.noreply.github.com>
2024-01-11 12:26:32 +0000

    new file:   src/app/account/wallet/wallet3/page.tsx 	new file:   src/app/layout/layout.jsx 	new file:   src/app/lib/web3wallet.jsx 	modified:   tsconfig.json

---
+++ src/app/layout/layout.jsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+import { Web3Modal } from "../lib/web3wallet";
+
+export const metadata = {
+  title: "Web3Modal",
+  description: "Web3Modal Example",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Web3Modal>{children}</Web3Modal>
+      </body>
+    </html>
+  );
+}
